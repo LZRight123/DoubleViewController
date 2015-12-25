79417ce5266e49f5c98df1ff5327d313fb33501a
Right <350442340@qq.com>
2015-12-25 11:49:45 +0800

    要

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DoubleViewController
+//
+//  Created by Right on 15/12/18.
+//  Copyright © 2015年 Right. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
